f50b5695072b715d968e632368e117410a71365f
Min Park <minpark815@gmail.com>
2018-12-11 14:24:27 -0600

    Update shell and notes

--- shell_scripts/available_output_n.sh
+++ shell_scripts/available_output_n.sh
@@ -3,6 +3,6 @@
 cd ../node_scripts
 for i in {0..10}
 do
-    node available_points.js > ../output/available_time_$i.out;
-    python ../python_scripts/available_points.py ../output/available_time_$i.out &
+    node available_points.js > ../output/active_available_time_$i.out;
+    python ../python_scripts/available_points.py ../output/active_available_time_$i.out &
 done
